8c68146b4c2a7fe124dfd85d7a276ee9935999ec
lucashthiele <lucasthiele0@outlook.com>
2024-05-10 19:13:31 -0300

    UserRepositoryTest findById method

--- src/test/java/com/lucashthiele/financial/repository/UserRepositoryTest.java
+++ src/test/java/com/lucashthiele/financial/repository/UserRepositoryTest.java
@@ -4,12 +4,10 @@ import com.lucashthiele.financial.models.user.User;
 import com.lucashthiele.financial.repositories.UserRepository;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.time.LocalDate;
 
@@ -19,23 +17,28 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+    private User user;
+
+    @BeforeEach
+    public void setupData() {
+        this.user = User.builder()
+                .id(null)
+                .email("test@test.com")
+                .password("12345678")
+                .firstName("First Name Test")
+                .surname("Surname Test")
+                .birthDate(LocalDate.parse("2000-01-01"))
+                .build();
+    }
 
     @Test
     public void UserRepository_SaveAll_ReturnsSavedUser() {
-            var user = User.builder()
-                    .id(null)
-                    .email("test@test.com")
-                    .password("12345678")
-                    .firstName("First Name Test")
-                    .surname("Surname Test")
-                    .birthDate(LocalDate.parse("2000-01-01"))
-                    .build();
-
-            var savedUser = userRepository.save(user);
+        var savedUser = userRepository.save(this.user);
 
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
     }
+
     /**
      * TODO - add new tests scenarios, need to evaluate all usages od the repository
      *  - Methods
@@ -46,4 +49,14 @@ public class UserRepositoryTest {
      *      findById({id}).orElseThrow
      *
      */
+
+    @Test
+    public void UserRepository_findById_ReturnUser() {
+        var savedUser = userRepository.save(this.user);
+
+        var retrievedUser = userRepository.findById(savedUser.getId()).get();
+
+        Assertions.assertThat(retrievedUser).isEqualTo(this.user);
+    }
+
 }
